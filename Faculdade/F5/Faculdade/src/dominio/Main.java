ea1cf58936e1aae3f02648fd484f4725e703096b
BodeDimitri <diegocesaralbino@gmail.com>
2024-03-12 19:41:34 -0300

    Dia 12/03

---
+++ Faculdade/F5/Faculdade/src/dominio/Main.java
@@ -0,0 +1,10 @@
+package dominio;
+
+public class Main {
+
+	public static void main(String[] args) {
+		cliente o = new cliente(null, null, null, null, null);
+
+	}
+
+}
---
+++ Faculdade/F5/Faculdade/src/dominio/cliente.java
@@ -0,0 +1,46 @@
+package dominio;
+import java.util.Date;
+
+public class cliente {
+	
+	protected String idUsuario;
+	public String nmCompleto;
+	public String dsEmail;
+	public Date dtAniversario;
+	private String txSenha;
+	
+	cliente(String idUsuario, String nmCompleto, String dsEmail, Date dtAniversario, String txSenha) {
+		super();
+		this.idUsuario = idUsuario;
+		this.nmCompleto = nmCompleto;
+		this.dsEmail = dsEmail;
+		this.dtAniversario = dtAniversario;
+		this.txSenha = txSenha;
+	}
+	
+	private String cadastrar(String nmCompleto, String dsEmail, Date dtAniversario, String txSenha) {
+		this.nmCompleto = nmCompleto;
+		this.dsEmail = dsEmail;
+		this.dtAniversario = dtAniversario;
+		this.txSenha = txSenha;
+		
+		return "Usuario criado com sucesso";
+	}
+	
+	private String exibirDados() {
+		return "Id: " + this.idUsuario + " ,Nome: " + this.nmCompleto + " ,Data de Aniversario: " + this.dtAniversario;
+	}
+	
+	private void alterarSenha(String novaSenha) {
+		this.txSenha = novaSenha;
+	}
+	
+	private String identificarSe(String idUsuario, String txSenha) {
+		if(idUsuario != this.idUsuario && txSenha != this.txSenha) {
+			return "Informacoes inseridas erradas, confira se digiou todas as informacoes corretamente";
+		} else {
+			return "Bem-vindo" + this.nmCompleto;
+		}
+		
+	}
+}
