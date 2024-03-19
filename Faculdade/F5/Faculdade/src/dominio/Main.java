fb0d662fd51fb824a451b8db9f438e192c571f27
BodeDimitri <diegocesaralbino@gmail.com>
2024-03-19 18:35:48 -0300

    Revert "Revert "Dia 19/03""
    This reverts commit 07bc0e41b9ef6b108d3b2f781057a1c7d86a819b.

--- Faculdade/F5/Faculdade/src/dominio/Main.java
+++ Faculdade/F5/Faculdade/src/dominio/Main.java
@@ -3,8 +3,20 @@ package dominio;
 public class Main {
 
 	public static void main(String[] args) {
-		cliente o = new cliente(null, null, null, null, null);
+		
+		
+		cliente cliente1 = new cliente(1, "Diego Sinhorini", "email@fiap.com.br", "28-07-2003", "password123");
+		cliente cliente2 = new cliente(2, "Ana Takata", "email@fiap.com.br", "19-02-1998", "password123");
+		tipoInvestimento cdi =  new tipoInvestimento(1, "CDI");
+		Investimento investimento1 = new Investimento(1, cliente1, cdi, "CDI", "Banco da Europa", 100.00, "18-02-2024", "18-02-2026");
 
+
+		
+		System.out.println(cliente1.exibirDados());
+		System.out.println(cliente2.exibirDados());
+		investimento1.exibirDados();
+		investimento1.alterar(1, "CDB", "Novo Banco MundoOpen", 200.0, "18-02-2024", "18-02-2028");
+		investimento1.exibirDados();
 	}
 
 }
